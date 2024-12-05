f06f6c174c943c280728a3a739dd4134f28c265c
z.gao1 <lukegao03@outlook.com>
2024-12-05 15:24:11 -0700

    From school

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import Ex1_Animals.Ex1_Main;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Inheritance project");
+        Ex1_Main.run();
+    }
+
+}
