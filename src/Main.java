7a2cdff1499f6615598169370a1dfaad0387808e
z.gao1 <lukegao03@outlook.com>
2025-01-06 15:25:42 -0700

    From school

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import Ex1_Animals.Ex1_Main;
 import Ex2_Restaurant.Ex2_Main;
 import Ex3_MusicFestival.Ex3_Main;
+import TwoD_Arrays.Ex1_GolfCard;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,7 +9,8 @@ public class Main {
         System.out.println("Inheritance Project");
 //        Ex1_Main.run();   //animals
 //        Ex2_Main.run();     //restaurant
-        Ex3_Main.run();
+//        Ex3_Main.run();
+        Ex1_GolfCard.run();
     }
 
 }
---
+++ src/TwoD_Arrays/Ex1_GolfCard.java
@@ -0,0 +1,18 @@
+package TwoD_Arrays;
+
+public class Ex1_GolfCard {
+
+    public static void run(){
+        String[] names = {"Bob", "Serena", "Gill", "Steve"};
+        int[][] scorecard = new int[4][18];
+        System.out.println(scorecard.length);
+        for(int row = 0; row < scorecard.length; row++){
+            for(int col = 0; col < scorecard[row].length; col++){
+                System.out.println(scorecard[row][col]);
+            }
+
+        }
+
+    }
+
+}
